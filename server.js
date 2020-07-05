26c90f8319777dfb9e57588ab0d71abbf912afa1
barrystone <barrystone3181@gmail.com>
2020-07-05 21:40:23 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const connectDB = require("./config/db");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("API Running"));
+
+//Connect Database
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
