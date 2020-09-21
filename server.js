57ce4aee64e59adf7957366e34f8e23a51638f3f
barrystone <barrystone3181@gmail.com>
2020-09-21 20:53:56 +0800

    install axios

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const connectDB = require("./config/db");
 const express = require("express");
+const connectDB = require("./config/db");
 const path = require("path");
 
 const app = express();
 
-//Connect Database
+// Connect Database
 connectDB();
 
-// Init Middleware (bodyparser)
+// Init Middleware
 app.use(express.json());
 
 // Define Routes
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
