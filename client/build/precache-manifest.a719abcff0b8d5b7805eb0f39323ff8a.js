6b29287290458c8346472e0074244582368205c4
barrystone <barrystone3181@gmail.com>
2020-09-21 20:41:35 +0800

    Fixed

---
+++ client/build/precache-manifest.a719abcff0b8d5b7805eb0f39323ff8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8baf4569192e701e903cecd9784b6153",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78fa81379abbfcfea3b2",
+    "url": "/static/css/main.7d9d1bb1.chunk.css"
+  },
+  {
+    "revision": "e5a76e52bce6e6e0021b",
+    "url": "/static/js/2.24765c6d.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.24765c6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78fa81379abbfcfea3b2",
+    "url": "/static/js/main.463139d5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
